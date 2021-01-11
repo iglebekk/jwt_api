c2435575ef78ad8714f79c8194c0e5d91599acb7
iglebekk <anders@iglebekk.no>
2021-01-11 22:32:20 +0100

    commeted out premature code

--- app/Http/Middleware/Token.php
+++ app/Http/Middleware/Token.php
@@ -36,32 +36,33 @@ class Token
             return response()->json(['status' => 'invalid bearer token'], 401);
         }
         
-        /**
-        * If not premium check request counting.
-        */
-        if(!$result->premium)
-        {
-            /**
-            * Checks if last request is the same as this month. If not it resets the counter.
-            */
-            if($result->mnt_req != $now->month)
-            {
-                $result->ant_req = 0;
-            }
+        // THIS MAY BE USED LATER. BAD HABIT? MAYBE...
+        // /**
+        // * If not premium check request counting.
+        // */
+        // if(!$result->premium)
+        // {
+        //     /**
+        //     * Checks if last request is the same as this month. If not it resets the counter.
+        //     */
+        //     if($result->mnt_req != $now->month)
+        //     {
+        //         $result->ant_req = 0;
+        //     }
             
-            /**
-            * Checks if maxium numbers of requests has been reached.
-            */
-            if($result->ant_req >= env('MAX_REQ'))
-            {
-                return response()->json(['status' => 'number of requests exceeded limit this month'], 401);
-            }
+        //     /**
+        //     * Checks if maxium numbers of requests has been reached.
+        //     */
+        //      if($result->ant_req >= env('MAX_REQ'))
+        //     {
+        //         return response()->json(['status' => 'number of requests exceeded limit this month'], 401);
+        //     }
             
-            /**
-            * Adds one to counted requests
-            */
-            $result->ant_req = $result->ant_req + 1;
-        }
+        //     /**
+        //     * Adds one to counted requests
+        //     */
+        //     $result->ant_req = $result->ant_req + 1;
+        // }
 
         /**
          * Converts the strClass to array and puts the result in $request to be used later
